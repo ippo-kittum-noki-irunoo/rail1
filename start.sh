06fd39673c7d1e108d4f92deadf1143816b01a5e
ippo-kittum-noki-irunoo <103121525+ippo-kittum-noki-irunoo@users.noreply.github.com>
2023-05-08 17:18:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/ippo-kittum-noki-irunoo/rail1.git /rail1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /rail1
 fi
-cd /Ajax
+cd /rail1
 pip3 install -U -r requirements.txt
 echo "Starting Hichoz....🔥"
 python3 bot.py
